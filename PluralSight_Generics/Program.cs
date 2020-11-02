f3d13f232651088d37534d160beaa4e9de9f6629
Johnny <jmccauley117@gmail.com>
2020-11-02 16:13:15 +0000

    Chapter 1 and 2 complete

--- PluralSight_Generics/Program.cs
+++ PluralSight_Generics/Program.cs
@@ -49,6 +49,46 @@ namespace PluralSight_Generics
                 Console.WriteLine(peeked.Name);
             }
 
+            Console.WriteLine("Results for hashsets");
+
+            HashSet<Employee> set = new HashSet<Employee>();
+            var emp = new Employee("Scott");
+            set.Add(emp);
+            set.Add(emp);
+
+
+            //When looping this structure the expectation will be that only one record will be returned. 
+            //this is because the hashSet only allows unique values to be added; 
+            foreach (var item in set) {
+                Console.WriteLine(item.Name); 
+            }
+
+            //Code for Dictionaries
+            Dictionary <string, Employee> employeeByName = new Dictionary<string, Employee>();
+            employeeByName.Add("Scott", new Employee("Scott"));
+            employeeByName.Add("Alan", new Employee("Alan"));
+            employeeByName.Add("Joy", new Employee("Joy"));
+
+
+            Employee scott = employeeByName["Scott"];
+
+            foreach (var item in employeeByName) {
+                Console.WriteLine("{0}:{1}", item.Key, item.Value.Name); 
+
+            }
+
+            //Inserting a list into a dictionary
+
+            Dictionary<string, List<Employee>> staff = new Dictionary<string, List<Employee>>();
+            staff.Add("Sales Team", new List<Employee> { new Employee("Steven"),
+                                                                new Employee("Travis"),
+                                                                new Employee("Shane")});
+
+            staff.Add("Engineering Team", new List<Employee> { new Employee("Shaun"),
+                                                               new Employee("James"),
+                                                                new Employee("Richard")}); 
+
+
 
         }
     }
